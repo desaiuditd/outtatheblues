de29e5f17703a882ff306421d3122c16df249f75
Manoj <mparihar@scu.edu>
2017-01-22 11:30:08 -0800

    Open new page on Click

---
+++ chrome-extention/graph.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Mj on 1/22/2017.
+ */
+
+google.charts.load('current', {'packages':['corechart']});
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Age', 'Weight'],
+    [ 8,      12],
+    [ 4,      5.5],
+    [ 11,     14],
+    [ 4,      5],
+    [ 3,      3.5],
+    [ 6.5,    7]
+  ]);
+
+  var options = {
+    "title": "Age vs. Weight comparison",
+    "hAxis": {"title": "Age", "minValue": 0, "maxValue": 15},
+    "vAxis": {"title": "Weight", "minValue": 0, "maxValue": 15},
+    "legend": "none"
+  };
+
+  var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(drawChart);
+
--- chrome-extention/popup1.js
+++ chrome-extention/popup1.js
@@ -1,6 +1,8 @@
 
 
   document.getElementById("vibes").addEventListener("click", function() {
-  var newURL = "file:///Users/mahendramhatre/Desktop/outtatheblues/chrome-extention/graph.html";
-  chrome.tabs.create({ url: newURL });
+  // var newURL = "file:///Users/mahendramhatre/Desktop/outtatheblues/chrome-extention/graph.html";
+    chrome.tabs.create({'url': '/graph.html'}, function(tab) {
+      // Tab opened.
+    });
 })
